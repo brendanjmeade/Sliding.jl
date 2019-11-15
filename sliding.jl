8734a865bf123b9cf343e6ef6fa257fc2ee0736a
brendanjmeade <brendanjmeade@gmail.com>
2019-11-15 17:15:24 -0500

    a, b evolution is almost ready

--- sliding.jl
+++ sliding.jl
@@ -11,17 +11,35 @@ function sliplaw(v, θ, dc)
     return -v * θ / dc * log(v * θ / dc)
 end
 
+function calcdadv()
+    dadv = 0
+    return dadv
+end
+
+function calcdvθclassic!(dvθ, vθ, p, t)
+    dc, η, σn, a, b, μ, Vp, L, ρ, statelaw = p
+    θ = vθ[1]
+    v = vθ[2]
+    dvθ[1] = statelaw(v, θ, dc)
+    dvθ[2] = 1 / (η / σn + a / v) * (μ * (Vp - v) / (L * σn) - b * dvθ[1] / θ)
+    return nothing
+end
+
+
 function calcdvθ!(dvθ, vθ, p, t)
     dc, η, σn, a, b, μ, vp, L, ρ, statelaw, fstar, vstar, θstar = p
     θ = vθ[1]
     v = vθ[2]
+    dθprev = dvθ[1]
+    dvprev = dvθ[2]
     dvθ[1] = statelaw(v, θ, dc)
-    dadt = 0
+    dadt = 0 # calcdadv(a, v) * dvprev
     dbdt = 0
-    numerator1 = (μ * (vp - v) / (L * σn) - b * dvθ[1] / θ)
-    numerator2 = 0
-    denominator = (η / σn + a / v)
-    dvθ[2] = (numerator1 + numerator2) / denominator
+    numclassic = (μ * (vp - v) / (L * σn) - b * dvθ[1] / θ)
+    numab = -dadt * log(v / vstar) - dbdt * log(θ / θstar)
+    numσn = 0
+    denom = (η / σn + a / v)
+    dvθ[2] = (numclassic + numab + numσn) / denom
     return nothing
 end
 
@@ -61,7 +79,7 @@ end
 function sliding()
     # Model parameters
     siay = 365.25 * 24 * 60 * 60
-    tspan = (0.0, siay * 5000.0)
+    tspan = (0.0, siay * 2000.0)
     μ = 3e10
     ν = 0.25
     ρ = 2700.0
@@ -80,14 +98,20 @@ function sliding()
     vstar = vp # ???
     θstar = 1e9 # ???
     
-    # Initial conditions
-    ics = [1e8; vp / 1000]
-    
-    # Time integrate
-    p = (dc, η, σn, a, b, μ, vp, L, ρ, aginglaw, fstar, vstar, θstar)
-    prob = ODEProblem(calcdvθ!, ics, tspan, p)
-    sol = solve(prob, RK4(), abstol = abstol, reltol = reltol)
-    plottimeseries(sol, siay, "RSF classic")
+    # Time integrate - classic
+    icsclassic = [1e8; vp / 1000]
+    pclassic = (dc, η, σn, a, b, μ, vp, L, ρ, aginglaw, fstar, vstar, θstar)
+    probclassic = ODEProblem(calcdvθclassic!, icsclassic, tspan, pclassic)
+    solclassic = solve(probclassic, RK4(), abstol = abstol, reltol = reltol)
+    plottimeseries(solclassic, siay, "RSF classic")
+
+    # Time integrate - a, b can evolve
+    icsab = [1e8 ; vp / 1000; a ; b]
+    # p = (dc, η, σn, a, b, μ, vp, L, ρ, aginglaw, fstar, vstar, θstar)
+    # prob = ODEProblem(calcdvθclassic!, ics, tspan, p)
+    # sol = solve(prob, RK4(), abstol = abstol, reltol = reltol)
+    # plottimeseries(sol, siay, "RSF classic")
+
     
     return nothing
 end
